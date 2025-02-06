e007638276468fd7e4d9691d93e416419cb32425
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:33:19 +0530

    Create a14.java

---
+++ a14.java
@@ -0,0 +1,15 @@
+// Conditional statement
+
+class a14
+{
+  public static void main(String args[])
+  {
+    int a = 10, b = 18;
+    String x;
+   
+    x = (a>b) ? "Pass" : "Fail";
+    
+     System.out.println("The result is: " + x);
+  }
+  
+}
