7ca1c579d919ab209a8cd70bc9e6678a5566ecbc
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:32:39 +0530

    Create arith.java

---
+++ arith.java
@@ -0,0 +1,20 @@
+/* Write Java program in which perform the arithmetic operations on two no's. */
+
+
+class arith
+{
+  public static void main(String args[])
+  {
+    int a = 10;
+    int b = 50;
+    
+    System.out.println("The Arithmetic operations on following no's:\n\n a = " + a + "\n b = " + b);
+    System.out.println("\n Addition: " + a + " + " + b + " = " + (a+b));
+    System.out.println(" Subtraction: " + a + " - " + b + " = " + (a-b));
+    System.out.println(" Multiplication: " + a + " * " + b + " = " + (a*b));
+    System.out.println(" Division: " + a + " / " + b + " = " + (a/b));
+    System.out.println(" Remainder: " + a + " % " + b + " = " + (a%b));
+  }
+}
+    
+    
