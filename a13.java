82c592bed2820a241d02c73b638d78d55382b955
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:33:47 +0530

    Create a13.java

---
+++ a13.java
@@ -0,0 +1,43 @@
+// Check statements
+
+class a13
+{
+  public static void main(String args[])
+  {
+    int a = 10, b = 20;
+    
+    
+    
+    if(a<=15 && b>=15)
+    {
+      System.out.println("Pass.");
+    }
+    else
+    {
+      System.out.println("Failed.");
+    }
+    
+    if(a<=5 ||  b>=30)
+    {
+      System.out.println("Pass.");
+    }
+    else
+    {
+      System.out.println("Failed.");
+    }
+    
+     if(! (a==b))
+    {
+      System.out.println("Pass.");
+    }
+    else
+    {
+      System.out.println("Failed.");
+    }
+  }
+}
+    
+    
+    
+         
+    
