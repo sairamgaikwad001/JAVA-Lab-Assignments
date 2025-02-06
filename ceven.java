76405c158b708550fa7a5ef96c1057286e806f42
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:32:14 +0530

    Create ceven.java

---
+++ ceven.java
@@ -0,0 +1,19 @@
+// print even nos between 1 to 100
+
+class ceven
+{
+  public static void main(String args[])
+  {
+    int i, n;
+    
+    System.out.println("\nThe even no's between 1 to 100 is: ");
+    for(i=1; i<100; i++)
+    {
+     if(i%2==0)
+     {
+      System.out.print(i + " ");
+    
+      }
+      }
+  }
+  }
