d8a418b922cdaa11414114ad6551d2f84047f626
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:24:49 +0530

    Create st.java

---
+++ st.java
@@ -0,0 +1,33 @@
+// check switch case
+
+class st
+{
+   public static void main(String args[])
+  {
+  
+   int ch = 2;
+   
+   
+   switch(ch)
+   { 
+     case 1:
+        System.out.println("Ram is Younger.");
+        break;
+        
+     case 2:
+        System.out.println("Shyam is Younger.");
+        break;
+        
+     case 3:
+        System.out.println("Roshan is Younger.");
+        break;
+        
+     case 4:
+       default:
+          System.out.println("Wrong Choice.");
+    }
+  }
+  
+}
+    
+    
