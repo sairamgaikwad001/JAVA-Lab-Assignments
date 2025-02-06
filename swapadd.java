47e027d16f8213c14123bd28efc32f1c7bd1081c
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:24:21 +0530

    Create swapadd.java

---
+++ swapadd.java
@@ -0,0 +1,20 @@
+/* Write Java program for swapping two no's without using 3rd variable.
+   Apply the logic of Addition and Subtraction. */
+
+
+class swapadd
+{
+  public static void main(String args[])
+  {
+    int a = 20;
+    int b = 45;
+    
+    System.out.println("Before Swapping:\n\na = " + a + "\nb = " + b);
+    
+    a = a+b;
+    b = a-b;
+    a = a-b;
+    
+    System.out.println("\nAfter Swapping:\n\na = " + a + "\nb = " + b);
+  }
+}
