6be57a6659246872a092538f14410a0e35a10519
sairamgaikwad001 <sairamgaikwad369@gmail.com>
2025-02-06 22:27:33 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,19 @@
+/* Write the Java program to check no is odd or even */
+
+class even
+{
+  public static void main(String args[])
+  {
+    int a = 13;
+    
+    if(a % 2 == 0)
+    {
+      System.out.println(a + " is even.");
+    }
+    
+    else
+    {
+      System.out.println(a + " is odd.");
+    }
+  }
+}
